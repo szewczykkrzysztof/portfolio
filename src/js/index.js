3e26c6d66d59359d2a10c7249cea9482c75d91dd
Krzysztof Szewczyk <szewczykris@gmail.com>
2021-01-20 15:34:38 +0100

    add js code to fetch my projects from github and deconstruct some of data from acquired json

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,22 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+const myProjects = document.querySelector(".myProjects__list--js");
 
-/* place your code below */
+// fetch my github respository list sorted from newest
+fetch("https://api.github.com/users/szewczykkrzysztof/repos?sort=created")
+  .then((resp) => resp.json())
+  .then((resp) => {
+    // for each element (repository) in json array
+    for (let repository of resp) {
+      // destructuring from object respository and extract properties
+      let { name, html_url, homepage, description } = repository;
+      //   if homepage doesn't exist link it to github project
+      if (homepage) {
+      } else {
+        homepage = html_url;
+      }
 
-console.log('HELLO 🚀')
+     
+    }
+  })
+  .catch(console.error("Błąd w pobieraniu danych z API Github"));
